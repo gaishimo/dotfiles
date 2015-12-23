d79f04bda730fe84ff09c95e99f25f77c6632f17
gaishimo <gaithimo@gmail.com>
2015-12-23 19:54:24 +0900

    Add diff-highlight

--- additional_settings.sh
+++ additional_settings.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 # ricty font
-cp -f /usr/local/Cellar/ricty/3.2.2/share/fonts/Ricty*.ttf ~/Library/Fonts/
+echo "copy ricty fonts"
+cp -frv /usr/local/Cellar/ricty/3.2.*/share/fonts/Ricty*.ttf ~/Library/Fonts/
 fc-cache -vf
+
+# git diff-highlight
+echo "install diff-highlight"
+curl https://raw.github.com/git/git/master/contrib/diff-highlight/diff-highlight -o /tmp/diff-highlight
+chmod +x /tmp/diff-highlight
+mv /tmp/diff-highlight $HOME/bin/diff-highlight
