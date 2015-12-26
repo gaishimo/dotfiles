ecd785ca96e73afa715bc1cb841f27f221308c66
gaishimo <gaithimo@gmail.com>
2015-12-26 13:06:39 +0900

    .

--- additional_settings.sh
+++ additional_settings.sh
@@ -10,3 +10,16 @@ echo "install diff-highlight"
 curl https://raw.github.com/git/git/master/contrib/diff-highlight/diff-highlight -o /tmp/diff-highlight
 chmod +x /tmp/diff-highlight
 mv /tmp/diff-highlight $HOME/bin/diff-highlight
+
+# settings repository
+mkdir -p $HOME/src/settings
+SETTINGS_PATH=$HOME/src/settings/my_settings
+git clone git@bitbucket.org:gaishimo/my_settings.git $SETTINGS_PATH
+
+# ssh
+cp $SETTINGS_PATH/ssh/keys/id* ~/.ssh/
+chmod 600 ~/.ssh/id_*
+cat $SETTINGS_PATH/ssh/configs/* >> ~/.ssh/config
+
+mkdir ~/.aws
+cp $SETTINGS_PATH/aws/credentials/gaishimo/* ~/.aws/
